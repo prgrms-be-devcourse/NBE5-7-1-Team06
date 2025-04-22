6fa000e6672bb54acaebc0e8ed7460bbc07537b2
sjsk3232 <sjsk3232@gmail.com>
2025-04-22 23:43:25 +0900

    first commit

---
+++ backend/src/test/java/kr/co/programmers/cafe/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.programmers.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
