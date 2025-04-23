57abb7d8fe74bd5a3c418b6bb34d60ab5d7017d1
KEEKE132 <dksvlfdhs1@gmail.com>
2025-04-23 12:40:30 +0900

    feat: add item create form

---
+++ backend/src/main/java/kr/co/programmers/cafe/items/dto/ItemCreateRequest.java
@@ -0,0 +1,15 @@
+package kr.co.programmers.cafe.items.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@NoArgsConstructor
+public class ItemCreateRequest {
+    private String name;
+    private String category;
+    private int price;
+    private MultipartFile image;
+    
+}
