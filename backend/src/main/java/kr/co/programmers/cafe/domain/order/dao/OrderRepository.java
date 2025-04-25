3659f1c91e6ad7708a8e1bc31cb313da7875168f
sjsk3232 <sjsk3232@gmail.com>
2025-04-25 19:38:58 +0900

    feat: 배송 대상 주문 검색 기능 구현

--- backend/src/main/java/kr/co/programmers/cafe/domain/order/dao/OrderRepository.java
+++ backend/src/main/java/kr/co/programmers/cafe/domain/order/dao/OrderRepository.java
@@ -1,7 +1,21 @@
 package kr.co.programmers.cafe.domain.order.dao;
 
 import kr.co.programmers.cafe.domain.order.entity.Order;
+import kr.co.programmers.cafe.domain.order.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT DISTINCT o FROM Order o " +
+            "LEFT JOIN FETCH o.orderItems oi " + // OrderItem 즉시 로딩
+            "WHERE o.orderedAt >= :start " +
+            "AND o.status = :status")
+    List<Order> findOrdersWithItemsByTimeAndStatus(
+            @Param("start") LocalDateTime start,
+            @Param("status") Status status
+    );
 }
