b9b81363cb5d8e08251d0f9652152f94e3a4f989
kwang2134 <kwang2134@naver.com>
2025-04-25 20:27:25 +0900

    refactor: CustomUserDetailsService 수정 - 코드 간소화

--- backend/src/main/java/kr/co/programmers/cafe/global/config/CustomUserDetailsService.java
+++ backend/src/main/java/kr/co/programmers/cafe/global/config/CustomUserDetailsService.java
@@ -14,38 +14,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserDetails userDetails;
     private final PasswordEncoder encoder;
     private final AdminProperties adminProperties;
 
     // yml 파일 기반 인증 정보를 담은 UserDetails 생성
     public CustomUserDetailsService(AdminProperties adminProperties, PasswordEncoder encoder) {
-
         this.encoder = encoder;
         this.adminProperties = adminProperties;
-
-        this.userDetails = User.builder()
-                .username(adminProperties.getName())
-                .password(encoder.encode(adminProperties.getPassword()))
-                .roles(adminProperties.getRoles())
-                .build();
     }
 
     // yml 파일 기반 인증 정보를 만들기 위해 Override
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!userDetails.getUsername().equals(username)) {
+        if (!username.equals(adminProperties.getName())) {
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
         }
-
-        boolean isAdmin = userDetails.getAuthorities().stream()
-                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
-
-        if (!isAdmin) {
-            log.warn("ADMIN 권한 부족");
-            throw new UsernameNotFoundException("권한이 부족합니다.");  // 로그인 실패 처리됨
-        }
-        log.info("Authorities = {}", userDetails.getAuthorities());
 
         return User.builder()
                 .username(adminProperties.getName())
