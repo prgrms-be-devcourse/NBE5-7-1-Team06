1b50d9ba3e7385a28f65aaedb00e13b95565835e
sjsk3232 <sjsk3232@gmail.com>
2025-04-25 20:41:53 +0900

    chore: import문 추가

--- backend/src/main/java/kr/co/programmers/cafe/domain/item/entity/Item.java
+++ backend/src/main/java/kr/co/programmers/cafe/domain/item/entity/Item.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.EntityListeners;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
