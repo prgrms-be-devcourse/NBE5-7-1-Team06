e1fe771e1e38334b84a071fbd3a36a0570e17623
limkanghyun <limkh06910691@gmail.com>
2025-04-25 17:09:52 +0900

    refactor: 주문 검색 성능 최적화 (@EntityGraph 사용)

--- backend/src/main/java/kr/co/programmers/cafe/domain/order/dao/OrderRepository.java
+++ backend/src/main/java/kr/co/programmers/cafe/domain/order/dao/OrderRepository.java
@@ -1,6 +1,7 @@
 package kr.co.programmers.cafe.domain.order.dao;
 
 import kr.co.programmers.cafe.domain.order.entity.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,10 +10,8 @@ import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("select o from Order o " +
-            "join fetch o.orderItems oi " +
-            "join fetch oi.item " +
-            "where o.id = :orderId")
+    @EntityGraph(attributePaths = {"orderItems", "orderItems.item"})
     Optional<Order> searchById(@Param("orderId") Long id);
 }
+
 
