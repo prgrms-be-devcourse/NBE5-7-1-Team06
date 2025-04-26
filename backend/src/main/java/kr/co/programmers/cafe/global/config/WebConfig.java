98c6ea4e518c53cd46c49a70f8c210d011180160
Heesu Song <bsthss0160@gmail.com>
2025-04-27 00:13:53 +0900

    refactor: API 연동 오류 발생 해결

--- backend/src/main/java/kr/co/programmers/cafe/global/config/WebConfig.java
+++ backend/src/main/java/kr/co/programmers/cafe/global/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
